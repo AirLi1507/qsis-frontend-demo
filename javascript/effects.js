d4a195fda1b85d142dd6274c62f186b54e93e4b9
AirLi1507 <167706999+AirLi1507@users.noreply.github.com>
2024-07-22 11:10:01 +0800

    Update effects.js

--- javascript/effects.js
+++ javascript/effects.js
@@ -1,47 +1,35 @@
-const squares = document.querySelectorAll('.sq');
-
-squares.forEach(Element => {
+document.querySelectorAll('.sq').forEach(Element => {
     Element.style = `
     width: var(--circle-size-1);
     height: var(--circle-size-1);
     border-radius: 0;
     margin: 0;`
-})
-
-document.addEventListener("mouseover", function(e) {
-  const hovering = e.target.id === 'logo' || e.target.closest('#logo');
-
-  if (hovering) {
-      squares.forEach(Element => {
-
-        Element.style.animation = "circle 0.5s ease 0s forwards normal";
-
-        Element.style = `
-        width: var(--circle-size-2);
-        height: var(--circle-size-2);
-        border-radius: 50%;
-        margin: auto;`;
-
-      });
-  }
-
 });
 
-document.addEventListener("mouseout", function(e) {
-  const unhovered = e.target.id === 'logo' || e.target.closest('#logo');
+$("#logo").mouseenter(function() {
+  document.querySelectorAll('.sq').forEach(Element => {
 
-  if (unhovered) {
-      squares.forEach(Element => {
+    Element.style.animation = "circle 0.5s ease 0s forwards normal";
 
-        Element.style.animation = "circle 0.5s ease 0s forwards reverse";
+    Element.style = `
+    width: var(--circle-size-2);
+    height: var(--circle-size-2);
+    border-radius: 50%;
+    margin: auto;`;
+
+  });
+});
+
+$("#logo").mouseleave(function() {
+  document.querySelectorAll('.sq').forEach(Element => {
+
+    Element.style.animation = "circle 0.5s ease 0s forwards reverse";
                       
-        Element.style = `
-        width: var(--circle-size-1);
-        height: var(--circle-size-1);
-        border-radius: 0;
-        margin: 0;`
+    Element.style = `
+    width: var(--circle-size-1);
+    height: var(--circle-size-1);
+    border-radius: 0;
+    margin: 0;`
 
-      });
-  }
-
+  });
 });
